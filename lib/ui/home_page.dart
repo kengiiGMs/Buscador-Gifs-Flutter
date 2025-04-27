baca3caffb7a958b8f4c94cbcb3000721260c227
kengiiGMs <kaikiaoto22@gmail.com>
2025-04-26 22:01:14 -0300

    feat: Structure API KEY usagefrom local .env file

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -1,9 +1,47 @@
 import 'package:buscador_gifs/ui/gif_page.dart';
 import 'package:flutter/material.dart';
 import 'package:transparent_image/transparent_image.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  String key = "";
+  String defaultURL = "";
+  String specificURL = "";
+
+  String? _search;
+  int _offset = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    getFileEnv();
+  }
+
+  Future<void> getFileEnv() async {
+    await dotenv.load(fileName: ".env");
+    setState(() {
+      key = dotenv.env['API_KEY'] ?? "";
+    });
+    setUrls();
+  }
+
+  setUrls() {
+    setState(() {
+      if (key != "") {
+        defaultURL =
+            "https://api.giphy.com/v1/gifs/trending?api_key=$key&limit=20&offset=20&rating=g&bundle=messaging_non_clips";
+        specificURL =
+            "https://api.giphy.com/v1/gifs/search?api_key=$key&q=$_search&limit=19&offset=$_offset&rating=g&lang=en&bundle=messaging_non_clips";
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -82,6 +120,8 @@ class HomePage extends StatelessWidget {
                     );
                   },
                 ),
+                Text(defaultURL),
+                Text(specificURL),
               ],
             ),
           ),
